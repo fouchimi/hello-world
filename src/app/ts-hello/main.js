3abf996ea14bc861e113ad661907b36b526953f8
= <ousmanefouchimi@gmail.com>
2018-02-26 20:05:00 -0700

    Declaring value using let keyword

--- src/app/ts-hello/main.js
+++ src/app/ts-hello/main.js
@@ -3,3 +3,10 @@ function log(message) {
 }
 var message = 'Hello World';
 log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally " + i);
+}
+doSomething();
--- src/app/ts-hello/main.ts
+++ src/app/ts-hello/main.ts
@@ -5,3 +5,12 @@ function log(message) {
 var message = 'Hello World';
 
 log(message);
+
+function doSomething() {
+ for(let i = 0; i < 5; i++){
+    console.log(i);
+ }
+ //console.log("Finally " + i);
+}
+
+doSomething();
