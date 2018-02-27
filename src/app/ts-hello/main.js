c070655b3aba4e5f00a53a399852a4de99e831d1
= <ousmanefouchimi@gmail.com>
2018-02-26 20:14:26 -0700

    Declare an enum in TypeScript

--- src/app/ts-hello/main.js
+++ src/app/ts-hello/main.js
@@ -7,6 +7,15 @@ function doSomething() {
     for (var i = 0; i < 5; i++) {
         console.log(i);
     }
-    console.log("Finally " + i);
+    //console.log("Finally " + i);
 }
 doSomething();
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Black"] = 2] = "Black";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Green;
+console.log(backgroundColor);
--- src/app/ts-hello/main.ts
+++ src/app/ts-hello/main.ts
@@ -1,16 +1,4 @@
-function log(message) {
-  console.log(message);
-}
 
-var message = 'Hello World';
-
-log(message);
-
-function doSomething() {
- for(let i = 0; i < 5; i++){
-    console.log(i);
- }
- //console.log("Finally " + i);
-}
-
-doSomething();
+enum Color { Green = 0, Red = 1, Black = 2 };
+let backgroundColor = Color.Green;
+console.log(backgroundColor);
