5ad3758bc8502c7218c594e3a1c174770e4ffb0f
= <ousmanefouchimi@gmail.com>
2018-02-26 21:19:26 -0700

    Applied concept of modules in TypeScript

--- src/app/ts-hello/main.js
+++ src/app/ts-hello/main.js
@@ -1,32 +1,7 @@
-var Point = (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    Point.prototype.draw = function () {
-        console.log('X: ' + this.x + ', Y: ' + this.y);
-    };
-    Object.defineProperty(Point.prototype, "setX", {
-        set: function (val) {
-            this.x = val;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Point.prototype, "getX", {
-        get: function () {
-            return this.x;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Point.prototype.getDistance = function (another) {
-        return Math.sqrt(Math.pow((another.x - this.x), 2) + Math.pow((another.y - this.y), 2));
-    };
-    return Point;
-}());
-var pointA = new Point(4, 5);
-var pointB = new Point(2, 1);
+"use strict";
+var point_1 = require('./point');
+var pointA = new point_1.Point(4, 5);
+var pointB = new point_1.Point(2, 1);
 console.log(pointA.getX);
 pointA.setX = 5;
 pointA.draw();
--- src/app/ts-hello/main.ts
+++ src/app/ts-hello/main.ts
@@ -1,25 +1,8 @@
-class Point {
-    constructor(private x?: number, private y?: number) {}
 
-    draw() {
-        console.log('X: ' + this.x + ', Y: ' + this.y);
-    }
+import { Point } from './point';
 
-    set setX(val) {
-        this.x = val;
-    }
-
-    get getX() {
-        return this.x;
-    }
-
-    getDistance(another: Point) {
-        return Math.sqrt(Math.pow((another.x - this.x), 2) + Math.pow((another.y - this.y), 2));
-    }
-}
-
-let pointA: Point = new Point(4, 5);
-let pointB: Point = new Point(2, 1);
+const pointA: Point = new Point(4, 5);
+const pointB: Point = new Point(2, 1);
 console.log(pointA.getX);
 pointA.setX = 5;
 
