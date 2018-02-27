5075cb2ef2e802b0f652b256390c3ebf3484d56b
= <ousmanefouchimi@gmail.com>
2018-02-27 14:34:16 -0700

    Added pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -16,13 +16,27 @@ import { CoursesService } from './courses.service';
                    <input type="text" #email placeholder="Enter text here" (keyup.enter)="onKeyUp(email.value)" />
                    <input type="text" [value]="name" placeholder="Enter text here" (keyup.enter)="name = $event.target.value; onPress()" />
                    <input type="text" [(ngModel)]="firstName" (keyup.enter)="onSubmit()" />
-                </div>`
+                </div>
+                <p>
+                  <span>{{ currentCourse.title | uppercase }}</span><br/>
+                  <span>{{ currentCourse.rating | number:'1.2-2' }}</span><br/>
+                  <span>{{ currentCourse.students | number }}</span><br/>
+                  <span>{{ currentCourse.price | currency:'AUD':true:'2.2-2' }}</span><br/>
+                  <span>{{ currentCourse.date | date:'shortDate' }}</span>
+                </p>`
 })
 export class CoursesComponent {
     private name = 'ousmanefouchimi@gmail.com';
     private firstName = 'Fouchimi';
     title = 'List of courses';
     courses;
+    currentCourse = {
+        title : 'The best Angular course ',
+        rating : 4.8947,
+        students : 40751,
+        price : 10.00,
+        date : new Date(2016, 3, 1)
+    };
     isActive = false;
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
