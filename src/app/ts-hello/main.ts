4f0a6aa75bc85a663fefb37a94db283ac22ee794
= <ousmanefouchimi@gmail.com>
2018-02-26 21:00:38 -0700

    Added concept of access modifiers and constructors

--- src/app/ts-hello/main.ts
+++ src/app/ts-hello/main.ts
@@ -1,6 +1,11 @@
 class Point {
-    x: number;
-    y: number;
+    private x: number;
+    private y: number;
+
+    constructor(x?: number, y?: number) {
+        this.x = x;
+        this.y = y;
+    }
 
     draw() {
         console.log('X: ' + this.x + ', Y: ' + this.y);
@@ -11,14 +16,9 @@ class Point {
     }
 }
 
-let pointA: Point = new Point();
-let pointB: Point = new Point();
-pointA.x = 4;
-pointA.y = 5;
+let pointA: Point = new Point(4, 5);
+let pointB: Point = new Point(2, 1);
 
 pointA.draw();
-
-pointB.x = 2;
-pointB.y = 1;
 
 console.log(pointA.getDistance(pointB));
