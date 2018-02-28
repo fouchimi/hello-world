0024905348e05e04ab162fdef0fc4c0ea4d38c71
= <ousmanefouchimi@gmail.com>
2018-02-27 19:24:02 -0700

    Added custom output properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,8 @@ export class AppComponent {
     title: 'Title',
     isFavorite: true
   };
+
+  favoriteChanged() {
+    console.log('Favorite value changed!');
+  }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-favorite',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FavoriteComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
   @Input('is-favorite') isFavorite = true;
+  @Output() change = new EventEmitter();
   constructor() {
   }
 
@@ -16,6 +17,7 @@ export class FavoriteComponent implements OnInit {
 
   onClick() {
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
 
 }
